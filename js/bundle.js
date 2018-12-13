5bca09095b22d97a9f1f636e0b3fffe8cec4a262
Lea <gratefulheartdesignza@gmail.com>
2018-12-13 10:58:18 +0200

    adds animation to front page flash cards, moves javascript for music player to only run on single playlist pages when premium, not ideal because it is inline

--- js/bundle.js
+++ js/bundle.js
@@ -32,7 +32,12 @@ $(document).ready(function(){
 $(document).ready(function() {
 
   console.log("js is working");
-  
+
+  // FADE IN FLASH CARDS ON HOME PAGE
+   $('.flash-card-array').hide().removeClass('hidden').slideDown(200);
+
+  $('.flash-card').hide().delay(100).fadeIn(400);
+
   // TOGGLE NAVIGATION LIST AND NAVIGATION TOGGLE
   $('#navToggle').on('click', function(e) {
 
@@ -46,44 +51,9 @@ $(document).ready(function() {
     }
   });
 
-  // PLAY-BUTTON FUNCTIONS FOR SINGLE PLAYLISTS. KEEP HIDDEN ON PAGE LOAD
 
-  $('.sample').hide().removeClass('hidden');
-
-      // show text on mouseover
-
-  $('.play').on('mouseover', function(e){
-    $('.sample').slideDown(300);
-  });
-
-  // toggle between play and pause buttons on click
-  $('.play').on('click', function(e) {
-    $(this).toggleClass('fa-pause fa-play');
-
-  });
-
- //EXPERIMENTAL APLAYER code
-
- $('.expand').hide().removeClass('hidden');
-
- $('.hover-expand').on('mouseover', function(e){
-   $('.expand').slideDown(300).addClass('expanded');
-   $('.fa-play').hide(300);
-
- });
-
- const ap = new APlayer({
-   container: document.querySelector('#aplayer'),
-   audio: [{
-     name: document.querySelector('#playerTitle').innerHTML,
-     artist: document.querySelector('#playerArtist').innerHTML,
-     url: document.querySelector('.sample-track').innerHTML,
-     //cover: document.querySelector('#playerCover').src
-   }]
- });
 
   // BACK BUTTON FUNCTION
-
 
   $('.back-button').on('click', function(e) {
     window.history.back();
